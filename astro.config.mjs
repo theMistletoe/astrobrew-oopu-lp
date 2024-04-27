7b96fb9776014795f38c8f932b00999c8c778061
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-04-27 14:17:25 +0000

    Astro.config.mjsとpackage-lock.jsonに新しいパッケージを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify/functions";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  })]
 });
